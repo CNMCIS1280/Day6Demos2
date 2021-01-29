6cfe2d110494991d89dfbb5582588cc84f28d740
Rob Garner <rgarner011235@gmail.com>
2021-01-28 19:09:10 -0700

    Add project files.

---
+++ Day4Demos/InventoryItem.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day4Demos
+{
+    public class InventoryItem
+    {
+        private static double _maxWeight;
+        public static void SetMaxWeight(double maxWeight)
+        {
+            _maxWeight = maxWeight;
+        }
+        public static double GetMaxWeight()
+        {
+            return _maxWeight;
+        }
+       
+
+        private int id;
+        public void SetId(int id)
+        {
+            this.id = id;
+        }
+        public int GetId()
+        {
+            return id;
+        }
+        
+        private int serialNumber;
+        public void SetSerialNumber(int serialNumber)
+        {
+            this.serialNumber = serialNumber;
+        }
+        public int GetSerialNumber()
+        {
+            return serialNumber;
+        }
+
+        private string manufacturer;
+        public void SetManufacturer(string manufacturer)
+        {
+            this.manufacturer = manufacturer;
+        }
+        public string GetManufacturer()
+        {
+            return manufacturer;
+        }
+
+        private int modelNum;
+        public void SetModelNum(int modelNum)
+        {
+            this.modelNum = modelNum;
+        }
+        public int GetModelNum()
+        {
+            return modelNum;
+        }
+
+        private int locationId;
+        public void SetLocationId(int locationId)
+        {
+            this.locationId = locationId;
+        }
+        public int GetLocationId()
+        {
+            return locationId;
+        }
+
+        private string condition;
+        public void SetCondition(string condition)
+        {
+            this.condition = condition;
+        }
+        public string GetCondition()
+        {
+            return condition;
+        }
+
+
+        private double weight;
+        public void SetWeight(double weight)
+        {
+            this.weight = weight;
+            CalcWeightOK();
+        }
+        public double GetWeight()
+        {
+            return weight;            
+        }
+
+        private int? ordernum;
+        public void SetOrderNum(int? ordernum)
+        {
+            this.ordernum = ordernum;
+        }
+        public int? GetOrderNum()
+        {
+            return ordernum;
+        }
+
+        //Lazy way
+        //public bool WeightOK()
+        //{
+        //    return weight < _maxWeight;
+        //}
+
+        //Eager
+        private bool weightOK;
+
+        public bool GetWeightOK()
+        {
+            return weightOK;
+        }
+
+        private void CalcWeightOK()
+        {
+            weightOK = weight < _maxWeight;
+        }
+
+        public string GetDescription()
+        {
+            string info = $"Item serialNumber: {serialNumber}\n";
+            info += $"Item location: {locationId}\n";
+            info += "Weigh OK? " + weightOK;
+            return info;
+        }
+    }
+}
---
+++ Day4Demos/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day4Demos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InventoryItem.SetMaxWeight(1000);
+
+            //Instantiate a list
+            List<InventoryItem> items = new List<InventoryItem>();
+
+            //Instantiate and item
+            InventoryItem item1 = new InventoryItem();
+            //Initializing teh item
+            item1.SetSerialNumber(1);
+            item1.SetWeight(500);
+            //Add item to list
+            items.Add(item1);
+
+            //Instantiate and item
+            InventoryItem item2 = new InventoryItem();
+            //Initializing teh item
+            item2.SetSerialNumber(2);
+            item2.SetWeight(300);
+            //Add item to list
+            items.Add(item2);
+
+            
+
+            Console.WriteLine("Count: " + items.Count);
+            Console.WriteLine("Capacity: " + items.Capacity);
+            Console.WriteLine("Type: "+ items);
+            foreach(InventoryItem item in items)
+            {
+                Console.WriteLine($"Desc: " + item.GetDescription());
+            }
+        }
+
+        
+    }
+}
